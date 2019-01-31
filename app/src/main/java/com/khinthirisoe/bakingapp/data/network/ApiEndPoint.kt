c052d8b55c8001af001b8958ad19d124111f108a
Khin <khinthirisoe.mdy@gmail.com>
2019-01-31 18:00:23 +0700

    add api endpoint

--- app/src/main/java/com/khinthirisoe/bakingapp/data/network/ApiEndPoint.kt
+++ app/src/main/java/com/khinthirisoe/bakingapp/data/network/ApiEndPoint.kt
@@ -2,7 +2,7 @@ package com.khinthirisoe.bakingapp.data.network
 
 object ApiEndPoint {
 
-    const val BASE_URL = "https://d17h27t6h515a5.cloudfront.net"
+    const val BASE_URL = "https://d17h27t6h515a5.cloudfront.net/"
     const val GET_RECEIPE = "topher/2017/May/59121517_baking/baking.json"
 
 }
