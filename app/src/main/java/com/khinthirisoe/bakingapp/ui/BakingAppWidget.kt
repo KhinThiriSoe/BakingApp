50af1b51172f3116e6ced0a06617f722a7b2bc49
Khin <khinthirisoe.mdy@gmail.com>
2019-02-08 11:48:34 +0700

    add widget

---
+++ app/src/main/java/com/khinthirisoe/bakingapp/ui/BakingAppWidget.kt
@@ -0,0 +1,54 @@
+package com.khinthirisoe.bakingapp.ui
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import com.khinthirisoe.bakingapp.R
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [BakingAppWidgetConfigureActivity]
+ */
+class BakingAppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            BakingAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+
+            val widgetText = BakingAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.baking_app_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
