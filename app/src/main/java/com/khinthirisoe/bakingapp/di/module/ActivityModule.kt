c257e455a6db8c0dbd838f740643766794d9843f
Khin <khinthirisoe.mdy@gmail.com>
2019-02-26 10:30:27 +0700

    rename apiService and apiUrl

--- app/src/main/java/com/khinthirisoe/bakingapp/di/module/ActivityModule.kt
+++ app/src/main/java/com/khinthirisoe/bakingapp/di/module/ActivityModule.kt
@@ -2,7 +2,7 @@ package com.khinthirisoe.bakingapp.di.module
 
 import android.app.Activity
 import android.content.Context
-import com.khinthirisoe.bakingapp.data.network.ApiHelper
+import com.khinthirisoe.bakingapp.data.network.ApiService
 import com.khinthirisoe.bakingapp.di.context.ActivityContext
 import com.khinthirisoe.bakingapp.ui.baking.BakingContract
 import com.khinthirisoe.bakingapp.ui.baking.model.BakingRepository
@@ -23,7 +23,7 @@ class ActivityModule(private val activity: Activity) {
     }
 
     @Provides
-    fun bakingRepository(apiHelper: ApiHelper): BakingRepository = BakingRepository(apiHelper)
+    fun bakingRepository(apiService: ApiService): BakingRepository = BakingRepository(apiService)
 
     @Provides
     fun bakingPresenter(repository: BakingRepository): BakingContract.Presenter = BakingPresenter(repository)
--- app/src/main/java/com/khinthirisoe/bakingapp/ui/baking/model/BakingRepository.kt
+++ app/src/main/java/com/khinthirisoe/bakingapp/ui/baking/model/BakingRepository.kt
@@ -1,14 +1,14 @@
 package com.khinthirisoe.bakingapp.ui.baking.model
 
 import com.khinthirisoe.bakingapp.data.model.Recipe
-import com.khinthirisoe.bakingapp.data.network.ApiHelper
+import com.khinthirisoe.bakingapp.data.network.ApiService
 import io.reactivex.Single
 import javax.inject.Inject
 
 class BakingRepository @Inject
 
-constructor(private val apiHelper: ApiHelper) {
+constructor(private val apiService: ApiService) {
 
-    fun fetchRecipes(): Single<ArrayList<Recipe>> = apiHelper.getBakingRecipes()
+    fun fetchRecipes(): Single<ArrayList<Recipe>> = apiService.getBakingRecipes()
 
 }
