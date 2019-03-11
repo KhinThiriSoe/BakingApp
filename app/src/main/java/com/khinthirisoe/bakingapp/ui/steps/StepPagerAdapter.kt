7658e36857e1feb877bb394fbd28b5a64fed2212
Khin <khinthirisoe.mdy@gmail.com>
2019-03-11 16:01:11 +0700

    change steplists to steps

--- app/src/main/java/com/khinthirisoe/bakingapp/ui/steps/StepPagerAdapter.kt
+++ app/src/main/java/com/khinthirisoe/bakingapp/ui/steps/StepPagerAdapter.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.khinthirisoe.bakingapp.data.model.Step
 
-class StepPagerAdapter(fragmentManager: FragmentManager, private var stepList: ArrayList<Step>) :
+class StepPagerAdapter(fragmentManager: FragmentManager, private var steps: ArrayList<Step>) :
     FragmentStatePagerAdapter(fragmentManager) {
 
     override fun getItem(position: Int): Fragment {
-        return StepVideoFragment.newInstance(stepList[position])
+        return StepVideoFragment.newInstance(steps[position])
     }
 
     override fun getCount(): Int {
-        return stepList.size
+        return steps.size
     }
 }
