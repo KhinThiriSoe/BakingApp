9bfa708b228795ea4d06f73531e95cd20c8a99d8
Khin <khinthirisoe.mdy@gmail.com>
2019-04-09 14:39:23 +0700

    add baking activity test

--- app/src/androidTest/java/com/khinthirisoe/bakingapp/BakingActivityTest.kt
+++ app/src/androidTest/java/com/khinthirisoe/bakingapp/BakingActivityTest.kt
@@ -1,9 +1,10 @@
 package com.khinthirisoe.bakingapp
 
-import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.khinthirisoe.bakingapp.ui.baking.view.BakingActivity
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,14 +13,16 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class BakingActivityTest{
 
-    @Rule
-    var activityTestRule = ActivityTestRule(BakingActivity::class.java, false, false)
+    @get:Rule
+    val activityTestRule = ActivityTestRule(BakingActivity::class.java)
 
+    @Before
+    fun launchActivity() {
+        ActivityScenario.launch(BakingActivity::class.java)
+    }
 
     @Test
     fun testClickOnAnyBakingItem() {
 
-        val intent = Intent()
-        activityTestRule.launchActivity(intent)
     }
 }
